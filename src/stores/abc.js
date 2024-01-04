9949b686386e3dabaefecb1c74f8bf41502e6dd6
通報した <peterjackson0107@gmail.com>
2024-01-04 23:08:32 +0800

    2308

--- src/stores/abc.js
+++ src/stores/abc.js
@@ -191,7 +191,7 @@ export default defineStore("data", {
             })
         },
         getWenhua() {
-            fetch('@/src/componments/data/bunnka')
+            fetch('../src/componments/data/bunnka')
                 .then(response => response.json())
                 .then(data => {
                     this.objb = data
@@ -199,7 +199,7 @@ export default defineStore("data", {
                 })
         },
         getSport() {
-            fetch('@/src/componments/data/api')
+            fetch('../src/componments/data/api')
                 .then(response => response.json())
                 .then(data => {
                     this.objs = data

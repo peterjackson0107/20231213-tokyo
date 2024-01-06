9069d6ccbf41bd555edd4ca95e85040d65363d5b
通報した <peterjackson0107@gmail.com>
2024-01-06 13:37:11 +0800

    202401061337

--- src/stores/abc.js
+++ src/stores/abc.js
@@ -191,7 +191,7 @@ export default defineStore("data", {
             })
         },
         getWenhua() {
-            fetch("bunnka.json")
+            fetch("../static/bunnka.json")
                 .then(response => response.json())
                 .then(data => {
                     this.objb = data
@@ -199,7 +199,7 @@ export default defineStore("data", {
                 })
         },
         getSport() {
-            fetch("api.JSON")
+            fetch("../static/api.JSON")
                 .then(response => response.json())
                 .then(data => {
                     this.objs = data
